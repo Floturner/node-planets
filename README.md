9791e57525f21a82a1b87d34f7ff5d7c74e7a733
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-02-21 12:04:20 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Planets Node.js project
+
+This project shows wow To `Read` CSV files in Node.js using NASA Kepler Mission data.
---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+const habitablePlanets = [];
+
+const isHabitablePlanet = (planet) => {
+	return (
+		planet.koi_disposition === 'CONFIRMED' &&
+		planet.koi_insol > 0.36 &&
+		planet.koi_insol < 1.11 &&
+		planet.koi_prad < 1.6
+	);
+};
+
+const logHabitablePlanetsName = (planets) =>
+	planets.forEach((planet, index) => {
+		console.log(`${index + 1} - ${planet.kepler_name}`);
+	});
+
+fs.createReadStream('./kepler_data.csv')
+	.pipe(
+		parse({
+			comment: '#',
+			columns: true,
+		})
+	)
+	.on('data', (data) => {
+		if (isHabitablePlanet(data)) {
+			habitablePlanets.push(data);
+		}
+	})
+	.on('error', (err) => {
+		console.error('Error ==>', err);
+	})
+	.on('close', () => {
+		console.log('-----------------------');
+		console.log('Habitable planets');
+		logHabitablePlanetsName(habitablePlanets);
+		console.log('-----------------------');
+	});
